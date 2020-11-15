53207a3cc4f90e51e5623e85471a4327dc6c2ff3
root <julien.roba@live.fr>
2020-11-15 16:05:12 +0100

    movement

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,13 @@
 import { Component } from 'react'
 import { Grid, styled } from '@material-ui/core'
 import grass from '../static/media/grass.PNG'
+import offPrusse from '../static/media/offPrusse.png'
 
 const SubTitleCard = styled(({ ...other }) => <Grid {...other} />)({
     height: '20px',
     width: '100%',
     backgroundColor: 'burlywood',
-    marginBottom: '20px',
+    marginBottom: (props) => (props.noMargin ? '0' : '20px'),
     border: '1px solid black',
 })
 
@@ -15,135 +16,395 @@ class Dashboard extends Component {
         super(props)
         this.state = {
             view: 4,
+            x: 5,
+            y: 5,
+            unit: {
+                id: 1,
+                name: 'Cendrov',
+                type: 1,
+                idUser: 1,
+                pa: 20,
+                view: 7,
+                eff: 50,
+                moral: 100,
+                xp: 0,
+                formation: 1,
+                x: 10,
+                y: 10,
+            },
+            movement: false,
         }
     }
 
-    getStyle = (size, i) => {
+    getStyle = (iUnit, i) => {
         return {
-            backgroundColor: i * 2 === size + 1 ? 'darkred' : i % 2 !== 0 ? 'grey' : 'lightgray',
+            backgroundColor: iUnit === i ? 'darkred' : i % 2 !== 0 ? 'grey' : 'lightgray',
             textAlign: 'center',
             width: '35px',
             height: '20px',
-            color: i * 2 === size + 1 ? 'white' : 'black',
-            fontWeight: i * 2 === size + 1 ? 'bold' : 'auto',
+            fontWeight: iUnit === i ? 'bold' : 'normal',
+            color: iUnit === i ? 'white' : 'black',
         }
     }
 
-    getView = () => {
-        const { view } = this.state
-        const size = view * 2 + 1
+    getRow = (xStart, y, unit, size) => {
         const rows = []
-        console.log(size)
-        for (let i = 1; i <= size; i++) {
-            rows.push(
-                <td
-                    style={{
-                        backgroundImage: `url(${grass})`,
-                        backgroundSize: 'cover',
-                        width: '45px',
-                        height: '45px',
-                    }}
-                />,
-            )
+        for (let i = xStart; i <= xStart + size; i++) {
+            if (i >= 0) {
+                if (y === unit.y && i === unit.x) {
+                    rows.push(
+                        <td
+                            style={{
+                                backgroundImage: `url(${grass})`,
+                                backgroundSize: 'cover',
+                                width: '45px',
+                                height: '45px',
+                            }}
+                        >
+                            <img
+                                src={offPrusse}
+                                style={{
+                                    width: '40px',
+                                    height: '40px',
+                                }}
+                                alt="unitImg"
+                            />
+                        </td>,
+                    )
+                } else {
+                    rows.push(
+                        <td
+                            style={{
+                                backgroundImage: `url(${grass})`,
+                                backgroundSize: 'cover',
+                                width: '45px',
+                                height: '45px',
+                            }}
+                        />,
+                    )
+                }
+            }
         }
+        return rows
+    }
+
+    getView = () => {
+        const { unit } = this.state
+        const xStart = unit.x - unit.view
+        const yStart = unit.y - unit.view
+        const size = unit.view * 2
         const firstRow = []
-        firstRow.push(<td style={this.getStyle(size, 0)} />)
-        for (let i = 1; i <= size; i++) {
-            firstRow.push(<td style={this.getStyle(size, i)}>{i}</td>)
+        firstRow.push(<td style={this.getStyle(unit.x, xStart + 1)} />)
+        for (let i = xStart; i <= xStart + size; i++) {
+            if (i >= 0) firstRow.push(<td style={this.getStyle(unit.x, i)}>{i}</td>)
         }
         const table = []
         table.push(firstRow)
-        for (let i = 1; i <= size; i++) {
-            table.push(
-                <tr>
-                    <td style={this.getStyle(size, i)}>{i}</td>
-                    {rows}
-                </tr>,
-            )
+        for (let i = yStart; i <= yStart + size; i++) {
+            if (i >= 0) {
+                table.push(
+                    <tr>
+                        <td style={this.getStyle(unit.y, i)}>{i}</td>
+                        {this.getRow(xStart, i, unit, size)}
+                    </tr>,
+                )
+            }
         }
         return (
-            <table className="no-spacing">
+            <table className="movement no-spacing">
                 <tbody>{table}</tbody>
             </table>
         )
     }
 
+    setMovement = (movement) => {
+        this.setState({ movement: parseInt(movement) })
+    }
+
+    moveUnit = () => {
+        const { unit, movement } = this.state
+        let moveX = 0
+        let moveY = 0
+        switch (movement) {
+            case 1:
+                moveX = -1
+                moveY = -1
+                break
+            case 2:
+                moveX = 0
+                moveY = -1
+                break
+            case 3:
+                moveX = 1
+                moveY = -1
+                break
+            case 4:
+                moveX = -1
+                moveY = 0
+                break
+            case 5:
+                moveX = 1
+                moveY = 0
+                break
+            case 6:
+                moveX = -1
+                moveY = 1
+                break
+            case 7:
+                moveX = 0
+                moveY = 1
+                break
+            case 8:
+                moveX = 1
+                moveY = 1
+                break
+            default:
+                break
+        }
+        if (movement) {
+            this.setState({
+                unit: {
+                    ...unit,
+                    x: unit.x + moveX,
+                    y: unit.y + moveY,
+                },
+                movement: false,
+            })
+        }
+    }
+
     render() {
-        const { view } = this.state
+        const { unit, movement } = this.state
 
         return (
-            <Grid
-                container
-                direction="row"
-                justify="flex-start"
-                alignItems="flex-start"
-                style={{ width: '100vw' }}
-                spacing={3}
-            >
-                <Grid item xs={3} container direction="column">
-                    <SubTitleCard item>
-                        <span
-                            style={{
-                                padding: '5px',
-                            }}
-                        >
-                            Infos du bataillon
-                        </span>
-                    </SubTitleCard>
-                    <SubTitleCard item>
-                        <span
-                            style={{
-                                padding: '5px',
-                            }}
-                        >
-                            Déplacement
-                        </span>
-                    </SubTitleCard>
-                    <input
-                        title="Vue"
-                        type="number"
-                        value={view}
-                        onChange={(e) => this.setState({ view: e.target.value })}
-                    />
-                    <SubTitleCard item>
-                        <span
-                            style={{
-                                padding: '5px',
-                            }}
-                        >
-                            Courrier du régiment
-                        </span>
-                    </SubTitleCard>
-                    <SubTitleCard item>
-                        <span
-                            style={{
-                                padding: '5px',
-                            }}
-                        >
-                            Mes derniers évenements
-                        </span>
-                    </SubTitleCard>
-                </Grid>
+            <>
+                <SubTitleCard item />
                 <Grid
-                    item
                     container
-                    direction="column"
-                    justify="center"
-                    alignItems="center"
-                    style={{ width: 'max-content' }}
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="flex-start"
+                    style={{ width: '100vw' }}
+                    spacing={3}
                 >
-                    <SubTitleCard>
-                        <span
-                            style={{
-                                padding: '5px',
-                            }}
+                    <Grid
+                        item
+                        xs={3}
+                        container
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                    >
+                        <SubTitleCard item noMargin>
+                            <span
+                                style={{
+                                    padding: '8px',
+                                }}
+                            >
+                                Infos du bataillon
+                            </span>
+                        </SubTitleCard>
+                        <Grid
+                            item
+                            container
+                            direction="row"
+                            style={{ padding: '10px' }}
+                            spacing={1}
                         >
-                            Front
-                        </span>
-                    </SubTitleCard>
-                    <Grid item>{this.getView()}</Grid>
+                            <Grid item xs={3}>
+                                Officier
+                            </Grid>
+                            <Grid item xs={9}>
+                                <select style={{ width: '100%' }}>
+                                    <option>Cendrov - EM</option>
+                                    <option>Centir - Gre.</option>
+                                    <option>Centor - Fus.</option>
+                                </select>
+                            </Grid>
+                            <Grid item xs={3}>
+                                Matricule
+                            </Grid>
+                            <Grid item xs={9}>
+                                {unit.id}
+                            </Grid>
+                            <Grid item xs={3}>
+                                Type
+                            </Grid>
+                            <Grid item xs={9}>
+                                {unit.type}
+                            </Grid>
+                            <Grid item xs={3}>
+                                XP
+                            </Grid>
+                            <Grid item xs={3}>
+                                {unit.xp}
+                            </Grid>
+                            <Grid item xs={3}>
+                                Effectif
+                            </Grid>
+                            <Grid item xs={3}>
+                                {unit.eff}
+                            </Grid>
+                            <Grid item xs={3}>
+                                PA
+                            </Grid>
+                            <Grid item xs={3}>
+                                {unit.pa}
+                            </Grid>
+                            <Grid item xs={3}>
+                                Moral
+                            </Grid>
+                            <Grid item xs={3}>
+                                {unit.moral}%
+                            </Grid>
+                        </Grid>
+                        <SubTitleCard item noMargin>
+                            <span
+                                style={{
+                                    padding: '8px',
+                                }}
+                            >
+                                Déplacement
+                            </span>
+                        </SubTitleCard>
+                        <Grid item>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 1}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={1}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 2}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={2}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 3}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={3}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 4}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={4}
+                                            />
+                                        </td>
+                                        <td />
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 5}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={5}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 6}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={6}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 7}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={7}
+                                            />
+                                        </td>
+                                        <td>
+                                            <input
+                                                type="radio"
+                                                name="move"
+                                                checked={movement === 8}
+                                                onChange={(e) => this.setMovement(e.target.value)}
+                                                value={8}
+                                            />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </Grid>
+                        <Grid item>
+                            <button type="button" onClick={this.moveUnit}>
+                                Déplacer le bataillon
+                            </button>
+                        </Grid>
+                        <input
+                            title="Vue"
+                            type="number"
+                            value={unit.view}
+                            onChange={(e) =>
+                                this.setState({ unit: { ...unit, view: parseInt(e.target.value) } })
+                            }
+                        />
+                        <SubTitleCard item noMargin>
+                            <span
+                                style={{
+                                    padding: '8px',
+                                }}
+                            >
+                                Courrier du régiment
+                            </span>
+                        </SubTitleCard>
+                        <SubTitleCard item noMargin>
+                            <span
+                                style={{
+                                    padding: '8px',
+                                }}
+                            >
+                                Mes derniers évenements
+                            </span>
+                        </SubTitleCard>
+                    </Grid>
+                    <Grid
+                        item
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                        style={{ width: 'max-content' }}
+                    >
+                        <SubTitleCard>
+                            <span
+                                style={{
+                                    padding: '8px',
+                                }}
+                            >
+                                Front
+                            </span>
+                        </SubTitleCard>
+                        <Grid item>{this.getView()}</Grid>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </>
         )
     }
 }
